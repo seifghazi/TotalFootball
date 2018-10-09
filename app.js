0faca07bd14a0104b2bd890305b5d16005567422
seifghazi <seifghazi@gmail.com>
2018-10-08 20:27:24 -0700

    refactor

--- app.js
+++ app.js
@@ -3,21 +3,14 @@ const app          = express();
 const request      = require('request');
 const bodyParser   = require('body-parser');
 const port         = process.env.PORT || 8000;
-const apiKey       = '9bdeeda8c6504d4d9b19c3d12e81c4fe';
-const url          = 'https://api.football-data.org/v2/competitions/CL/matches';
-
-
-var options = {
-  url: url,
-  headers: {
-    'X': apiKey
-  }
-};
-
-
+const requests     = require('./requests.js')
 
 app.get('/', function(req, res){
-  request(options, function(err, resp, body){
+  res.send('HOME PAGE');
+})
+
+app.get('/api', function(req, res){
+  request(requests.options, function(err, resp, body){
     if(!err && resp.statusCode == 200) {
             var parsedData = JSON.parse(body);
             res.send(parsedData);
---
+++ requests.js
@@ -0,0 +1,13 @@
+const apiKey    = require('./config.js');
+const url          = 'https://api.football-data.org/v2/competitions/CL/matches';
+
+var options = {
+  url: url,
+  headers: {
+    'X-Auth-Token': apiKey.apiKey
+  }
+};
+
+module.exports = {
+  options: options
+}
