83b3824df152b311650e2066ccb6ca5a22e97b32
seifghazi <seifghazi@gmail.com>
2018-12-23 14:47:18 +0300

    cleaned up test

--- public/scorerChart/scorer_chart.js
+++ public/scorerChart/scorer_chart.js
@@ -1,4 +1,3 @@
 let chartData = JSON.parse(document.getElementById('scorers').innerText);
-var ctx = document.getElementById("scorersChart").getContext('2d');
-
-var myChart = new Chart(ctx, chartData);
+let ctx = document.getElementById("scorersChart").getContext('2d');
+let myChart = new Chart(ctx, chartData);
--- spec/controllers_spec.js
+++ spec/controllers_spec.js
@@ -16,11 +16,9 @@ describe('chart object being populated given certain input', () => {
       }
     ];
     const chartObject = chartsController.scorersChartSetup(scorers);
-
     expect(chartObject.data.labels[0]).toEqual(scorers[0].player.name);
     expect(chartObject.data.datasets[0].data[0]).toEqual(parseInt(scorers[0].numberOfGoals));
-    expect(chartObject.data.labels.length).toBe(1);
-    expect(chartObject.data.datasets[0].data.length).toBe(1);
+    expect(chartObject.data.labels.length).toEqual(chartObject.data.datasets[0].data.length);
 
     scorers.push(
       {
@@ -30,12 +28,10 @@ describe('chart object being populated given certain input', () => {
         numberOfGoals: '1'
       }
     );
-    
-    const updatedChartObject = chartsController.scorersChartSetup(scorers);
 
+    const updatedChartObject = chartsController.scorersChartSetup(scorers);
     expect(updatedChartObject.data.labels[1]).toEqual(scorers[1].player.name);
     expect(updatedChartObject.data.datasets[0].data[1]).toEqual(parseInt(scorers[1].numberOfGoals));
-    expect(updatedChartObject.data.labels.length).toBe(2);
-    expect(updatedChartObject.data.datasets[0].data.length).toBe(2);
+    expect(chartObject.data.labels.length).toEqual(chartObject.data.datasets[0].data.length);
  })
 })
